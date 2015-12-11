2b84d940d230453d03590e85c6b64036b3e46ef2
Darkchicken <joshshucker@hotmail.com>
2015-12-10 18:22:29 -0800

    More stuff

--- Assets/Scripts/FlightControls.cs
+++ Assets/Scripts/FlightControls.cs
@@ -3,13 +3,25 @@ using System.Collections;
 
 public class FlightControls : MonoBehaviour {
 
-    float moveSpeed = 10f;
+    float xatstart = 0;
+    float yatstart = 0;
+
+    public float turnSpeed = 20f;
+
+    private Vector3 Move;
+
+
+
+    public float moveSpeed = 10f;
     private Quaternion localRotation; // 
-    public float speed = 1.0f; // ajustable speed from Inspector in Unity editor
+   // public float speed = 1.0f; // ajustable speed from Inspector in Unity editor
 
     // Use this for initialization
     void Start ()
     {
+        xatstart = Input.acceleration.x;
+        yatstart = Input.acceleration.y;
+
         // copy the rotation of the object itself into a buffer
         localRotation = transform.rotation;
     }
@@ -17,11 +29,12 @@ public class FlightControls : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
+       
         Debug.Log(Input.acceleration.y);
         transform.Translate(transform.forward * moveSpeed * Time.deltaTime);
 
         // find speed based on delta
-        float curSpeed = Time.deltaTime * speed;
+        float curSpeed = Time.deltaTime * turnSpeed;
         // first update the current rotation angles with input from acceleration axis
         
        
@@ -34,5 +47,6 @@ public class FlightControls : MonoBehaviour {
 
         // then rotate this object accordingly to the new angle
         transform.rotation = localRotation;
+        
     }
 }
