612585c75f84637dcbf8120e06b9c3140f77c892
Darkchicken <joshshucker@hotmail.com>
2015-12-25 10:47:05 -0800

    Added Cardboard SDK
    Added new cardboard movement
    Fixed flight controls

--- Assets/Scripts/FlightControls.cs
+++ Assets/Scripts/FlightControls.cs
@@ -9,7 +9,7 @@ public class FlightControls : MonoBehaviour {
     public float turnSpeed = 20f;
 
     private Vector3 Move;
-
+    public GameObject head; 
 
 
     public float moveSpeed = 10f;
@@ -19,34 +19,17 @@ public class FlightControls : MonoBehaviour {
     // Use this for initialization
     void Start ()
     {
-        xatstart = Input.acceleration.x;
-        yatstart = Input.acceleration.y;
-
-        // copy the rotation of the object itself into a buffer
-        localRotation = transform.rotation;
+        head = GameObject.Find("Head");
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
-       
-        Debug.Log(Input.acceleration.y);
-        transform.Translate(transform.forward * moveSpeed * Time.deltaTime);
 
-        // find speed based on delta
-        float curSpeed = Time.deltaTime * turnSpeed;
-        // first update the current rotation angles with input from acceleration axis
         
-       
-        localRotation.x += (Input.acceleration.y) * curSpeed;
-       
-        
-        localRotation.y += Input.acceleration.x * curSpeed;
-       
+        transform.Translate(head.transform.forward * moveSpeed * Time.deltaTime);
 
-
-        // then rotate this object accordingly to the new angle
-        transform.rotation = localRotation;
+      
         
     }
 }
